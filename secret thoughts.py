3a56d76b7f482e0a93674089ef0ccbdd7659f6da
icey1l <163438524+icey1l@users.noreply.github.com>
2024-03-14 23:47:27 +0800

    Add files via upload

---
+++ secret thoughts.py
@@ -0,0 +1,82 @@
+print("Hello ^-^")
+import time
+time.sleep(1)
+name = input("What's your name?  -")
+name = name.capitalize()
+print("Hi " + (name))
+time.sleep(1)
+print("Please only use small letters")
+time.sleep(1)
+joy = input("Are you sure you dont want to be called Joy?   -")
+if joy == "no":
+ joy1 = print("Wow d ko ini gin expect, pero tawagon ko gapon ikaw as how u introduce ur self")
+elif joy == "yes":
+    joy2 = print("labot")
+time.sleep(2.5)
+print("riddle me this")
+time.sleep(2)
+x = input("What did the paperclip say to the magnet?  -")
+y = str("i find you really attractive")
+while not x == y:
+    print("eeeng MALI!")
+    x = input("What did the paperclip say to the magnet?  -")
+time.sleep(1)
+print("Correct, was it challenging?")
+time.sleep(1)
+print("dont care HAHAHAH jk")
+time.sleep(2)
+print("Now for a puzzle")
+time.sleep(2)
+print("solve this problem")
+time.sleep(1.8)
+print("First word consist of 1 letter")
+time.sleep(1)
+q = input("CLUE: First person pronoun  -")
+w = "i"
+while not q == w:
+    print("Wrong")
+    q = input("CLUE: First person pronoun  -")
+
+print("Correct")
+time.sleep(1.5)
+
+print("2nd Word consists of 4 letters")
+time.sleep(1)
+e = input("CLUE: 12, 9, 11, 5  -")
+r = "like"
+while not e == r:
+    print("Wrong")
+    e = input("CLUE: 12, 9, 11, 5  -")
+print("Correct")
+time.sleep(1.5)
+print("3rd Word consists of 3 letters")
+time.sleep(1)
+t = input("Nasa pin message san convo ta  -")
+u = "you"
+while not t == u:
+    print("Wrong!")
+    t = input("Nasa pin message san convo ta  -")
+print("Correct!")
+time.sleep(1)
+print("I guess the cat is out of the bag")
+time.sleep(2)
+print("if u still dont get it well")
+time.sleep(2)
+print("I like you not as a friend but more")
+time.sleep(2)
+print("If the feeling is not mutal then its alright")
+time.sleep(3)
+print("It just means I can finally move on")
+time.sleep(2)
+print("Babye " + (name) )
+time.sleep(10)
+print("Oh your still here?")
+time.sleep(5)
+print("Why are you not leaving?")
+time.sleep(10)
+print("ngl im scared if u say yes and I fail to meet your standards")
+print("Im constantly thinking about this, making me rethink if I should confess")
+time.sleep(5)
+
+
+
